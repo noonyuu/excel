841fb2ac9b5ac273e94241442c12fa3b693ea0f6
shimizu <noon3m.op.sh@gmail.com>
2024-07-12 21:17:23 +0900

    [add]ログイン画面の雛形と画面サイズによる表示項目調整用の関数と各ルーティングを作成

---
+++ front/src/feature/login/page/Login.tsx
@@ -0,0 +1,11 @@
+import  pen  from './pengin.webp'
+
+export const Login = () => {
+  return (
+    <main>
+      <div className="">
+        <img className="size-full" src={pen} alt="" />
+      </div>
+    </main>
+  )
+}
--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  // 画面サイズが300px以下の場合は警告画面に切り替える
     <App />
-  </React.StrictMode>,
+  // </React.StrictMode>
 )
